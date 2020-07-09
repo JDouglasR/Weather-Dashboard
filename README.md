75f872eb8cafc797428f2004754ba7f08df1e494
Justin Roose <justin.d.roose@gmail.com>
2020-07-08 22:31:34 -0400

    Amended the readme --- fixed issues related to saved searches --- responsiveness needs fine-tuning

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Weather-Dashboard
+# Weather-Dashboard __*Weather Dashboard*__
+
+## __Detials About This Assignment__
+The purpose of this homework assignment was to use the OpenWeather API to retrieve weather data for cities and display it to a user in an organized and concise manner, using icons and labels to specify conditions.  Then, to use localStorage to store persistent data for later viewing.
+
+### __Link To Website__
+https://jdouglasr.github.io/Weather-Dashboard/
+
+### __Image of 'index.html'__
+![index.html](/img/weather-dashboard.png)
--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-    
-    // This function displays the weather to the page.
-    const displayCityWeather = () => {
 
-        // Variables
-        var todaysDate = moment().format('MMMM Do YYYY');
+	var todaysDate = moment().format('MMMM Do YYYY');
+	var cityList = $(".list-group");
+	var cityButtons = [];
+
+    // This function displays the weather to the page.
+    var displayCityWeather = (cityName) => {
+		
+		// Global Variables.
+		//cityName = $("#citySearch").val();
+        
         var apiKey = "561936218884bf45974f29319f32b882";
-        var cityName = $("#citySearch").val();
         var queryWeather = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey;
-        var queryForecast = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey;
+		var queryForecast = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey;
+		
 
         // This ajax call is used to gather weather data from an API and display it to the page.
         $.ajax({
@@ -87,7 +92,7 @@ $(document).ready(function() {
             url: queryForecast,
             method: "GET"
         }).then(function(responseForecast) {
-						//console.log(responseForecast);
+						console.log(responseForecast);
 
 						//This sets the date for corresponding days to variables.
 						var date1 = moment().add(1, 'days').format("MM/DD/YYYY");
@@ -97,11 +102,11 @@ $(document).ready(function() {
 						var date5 = moment().add(5, 'days').format("MM/DD/YYYY");
 
 						//This sets icons for corresponding days to variables.
-						var icon1 = "https://openweathermap.org/img/w/"+responseForecast.weather[4].icon+".png";
-						var icon2 = "https://openweathermap.org/img/w/"+responseForecast.weather[12].icon+".png";
-						var icon3 = "https://openweathermap.org/img/w/"+responseForecast.weather[20].icon+".png";
-						var icon4 = "https://openweathermap.org/img/w/"+responseForecast.weather[28].icon+".png";
-						var icon5 = "https://openweathermap.org/img/w/"+responseForecast.weather[36].icon+".png";
+						var icon1 = "https://openweathermap.org/img/w/"+responseForecast.list[4].weather[0].icon+".png";
+						var icon2 = "https://openweathermap.org/img/w/"+responseForecast.list[12].weather[0].icon+".png";
+						var icon3 = "https://openweathermap.org/img/w/"+responseForecast.list[20].weather[0].icon+".png";
+						var icon4 = "https://openweathermap.org/img/w/"+responseForecast.list[28].weather[0].icon+".png";
+						var icon5 = "https://openweathermap.org/img/w/"+responseForecast.list[36].weather[0].icon+".png";
 
 						//This sets temperature for corresponding days to variables.
 						var temp1 = `Temp: ${(((responseForecast.list[4].main.temp - 273.15) * 1.80 + 32).toFixed(2))} °F`;
@@ -125,11 +130,11 @@ $(document).ready(function() {
 						$("#date5").text(date5);
 						
 						//This displays icons for corresponding days to the page.
-						$("#icon1").attr("src", icon1).attr("alt", responseWeather.weather[4].description)
-						$("#icon2").attr("src", icon2).attr("alt", responseWeather.weather[12].description)
-						$("#icon3").attr("src", icon3).attr("alt", responseWeather.weather[20].description)
-						$("#icon4").attr("src", icon4).attr("alt", responseWeather.weather[28].description)
-						$("#icon5").attr("src", icon5).attr("alt", responseWeather.weather[36].description)
+						$("#icon1").attr("src", icon1).attr("alt", responseForecast.list[4].weather[0].description);
+						$("#icon2").attr("src", icon2).attr("alt", responseForecast.list[12].weather[0].description);
+						$("#icon3").attr("src", icon3).attr("alt", responseForecast.list[20].weather[0].description);
+						$("#icon4").attr("src", icon4).attr("alt", responseForecast.list[28].weather[0].description);
+						$("#icon5").attr("src", icon5).attr("alt", responseForecast.list[36].weather[0].description);
 
 						//This displays temperature for corresponding days to the page.
 						$("#temp1").text(temp1);
@@ -146,19 +151,68 @@ $(document).ready(function() {
 						$("#hum5").text(hum5);
 		});
 
-		// This is the function that saves recent searches.
-		const saveWeaterData = () => {
-			var cityList = $(".list-group");
+		
+		
+	};
 
-		};
+	// This function gets array of cities searched from local storage.
+	getCities = () => {
+        var storedCities = localStorage.getItem("Cities Searched For");
+
+        if (storedCities) {
+			cityButtons = JSON.parse(storedCities);
+			
+			renderCityBtn();
+			displayCityWeather(cityButtons[0]);
+		}
+		
+    }
+
+	// This function renders a button for each searched city.
+	const renderCityBtn = () => {
+
+		// This deletes cities prior to adding new ones.
+		$("#cityList").empty();
+
+		// This loops through array of cities.
+		for (var i = 0; i < cityButtons.length; i++){
+
+			var ctyBtn = $("<button>");
+
+			ctyBtn.attr("type", "button");
+			ctyBtn.attr("class", "list-group-item list-group-item-action cityButton");
+			ctyBtn.attr("data-cityName", cityButtons[i]);
+			ctyBtn.text(cityButtons[i])
+
+			$(cityList).append(ctyBtn);
+		}
+
+		// This sets the array of cities into local storage.
+		localStorage.setItem("Cities Searched For", JSON.stringify(cityButtons));
+
 	};
 
 	// This is a click event that runs the above functions when the search button is clicked.
     $("#searchBtn").on("click",function(event){
-        event.preventDefault();
-        displayCityWeather();
-    });
+		//event.preventDefault();
 
+		var cityName = $("#citySearch").val();
+		
+		// This adds city that was searched to an array.
+		cityButtons.push(cityName);
+
+		console.log(cityButtons);
+
+
+		renderCityBtn();
+		displayCityWeather(cityName);
+	});
+	
+	$(document).on("click", ".cityButton", function() {
+		displayCityWeather($(this).attr("data-cityName"));
+	});
+
+	getCities();
 
 });
 

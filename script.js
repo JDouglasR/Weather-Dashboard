334ceb75d8f4c8a7a72606ec1d9ca37b67eec374
Justin Roose <57727291+JDouglasR@users.noreply.github.com>
2020-07-13 15:55:21 -0400

    Update script.js
    added .reverse() method to line 166 so that the last item in the array is displayed first.

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ $(document).ready(function() {
 			cityButtons = JSON.parse(storedCities);
 			
 			renderCityBtn();
-			displayCityWeather(cityButtons[0]);
+			displayCityWeather(cityButtons[0].reverse());
 		}
 		
     }
